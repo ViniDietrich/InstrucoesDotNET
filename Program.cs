ba7e85917ce60cdb1b669afdcab0f588d792d475
Vinicius Dietrich <vin.dietrich@gmail.com>
2020-01-07 15:08:02 -0300

    Frost

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Intruções_dotNET
+{
+    class Program
+    {
+        static void Declaracoes()
+        
+        static void InstrucaoIf(string[] args)
+
+        static void InstrucaoSwitch(string[] args)
+       
+        static void InstrucaoWhile(string[] args)
+        
+        static void InstrucaoDo(string[] args)
+        
+        static void InstrucaoFor(string[] args)
+        
+        static void InstrucaoForeach(string[] args)
+        
+        
+        static void InstrucaoContinue(string[] args)
+        
+        static void InstrucaoReturn(string[] args)
+        
+        static void InstrucoesTryCatchFinallyThrow(string[] args)
+       
+        static void InstrucaoUsing(string[] args)
+
+    }
+}
